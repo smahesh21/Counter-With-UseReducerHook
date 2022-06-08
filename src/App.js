ee7a8bc76261a9f59446f6bae863f4ea1152aef5
Mahesh Srisailam <srisailammahesh77@gmail.com>
2022-06-08 06:40:25 +0000

    UseReducer Hook

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Counter from './components/CounterWithUseReducer'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <Counter />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CounterWithUseReducer/index.js
@@ -0,0 +1,35 @@
+import {useReducer} from 'react'
+
+const initialState = 0
+
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
+function Counter() {
+  const [count, dispatch] = useReducer(reducer, initialState)
+  return (
+    <div>
+      <div>Count - {count}</div>
+      <button type="button" onClick={() => dispatch('increment')}>
+        Increment
+      </button>
+      <button type="button" onClick={() => dispatch('decrement')}>
+        Decrement
+      </button>
+      <button type="button" onClick={() => dispatch('reset')}>
+        Reset
+      </button>
+    </div>
+  )
+}
+export default Counter
